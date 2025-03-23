888f7130e818fb4a1ed22a2f04210635b059d3ff
Aaditya Raikar <aadityaraikar73@gmail.com>
2025-03-23 20:53:18 +0530

    fix: fix read button by resolving checkLoginAndRedirect error

--- public/js/script.js
+++ public/js/script.js
@@ -13,65 +13,6 @@ btnNavEl.addEventListener('click', function () {
 });
 
 ///////////////////////////////////////////////////////////
-// Sticky navigation
-const sectionHeroEl = document.querySelector('.section-hero');
-
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-
-    if (ent.isIntersecting === false) {
-      document.body.classList.add('sticky');
-    }
-    if (ent.isIntersecting) {
-      document.body.classList.remove('sticky');
-    }
-  },
-  {
-    // In the viewport
-    root: null,
-    threshold: 0,
-    rootMargin: '-80px',
-  }
-);
-obs.observe(sectionHeroEl);
-
-///////////////////////////////////////////////////////////
-// Smooth scrolling animation
-const allLinks = document.querySelectorAll('a:link');
-
-allLinks.forEach(function (link) {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    const href = link.getAttribute('href');
-
-    // Scroll back to top
-    if (href == '#')
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-
-    // Scroll to other links
-    if (href !== '#' && href.startsWith('#')) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({
-        behavior: 'smooth',
-      });
-    }
-
-    // Navigate to different route
-    if (href.startsWith('/')) {
-      window.location.href = href;
-    }
-
-    // Close mobile navigation
-    if (link.classList.contains('main-nav-link'))
-      headerEl.classList.toggle('nav-open');
-  });
-});
-
-///////////////////////////////////////////////////////////
 document.addEventListener('DOMContentLoaded', async () => {
   const loginButton = document.querySelector('#login-btn');
   const signoutButton = document.querySelector('#signout-btn');
