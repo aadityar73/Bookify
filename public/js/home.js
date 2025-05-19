d0a1ab49727a6f50934ca1c60a6fa817115f1e8a
Aaditya Raikar <aadityaraikar73@gmail.com>
2025-05-20 03:21:24 +0530

    fix: enable footer links to open on click

--- public/js/home.js
+++ public/js/home.js
@@ -23,38 +23,3 @@ const obs = new IntersectionObserver(
   }
 );
 obs.observe(sectionHeroEl);
-
-///////////////////////////////////////////////////////////
-// Smooth scrolling animation
-const allLinks = document.querySelectorAll('a:link');
-
-allLinks.forEach(function (link) {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    const href = link.getAttribute('href');
-
-    // Scroll back to top
-    if (href == '#')
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-
-    // Scroll to other links
-    if (href !== '#' && href.startsWith('#')) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({
-        behavior: 'smooth',
-      });
-    }
-
-    // Navigate to different route
-    if (href.startsWith('/')) {
-      window.location.href = href;
-    }
-
-    // Close mobile navigation
-    if (link.classList.contains('main-nav-link'))
-      headerEl.classList.toggle('nav-open');
-  });
-});
