98447bf01fb4f796266dbbd61fd9f1c892d61396
Aaditya Raikar <124092166+aadityar73@users.noreply.github.com>
2025-10-01 09:49:52 +0530

    feat: add isAdmin middleware

---
+++ src/middlewares/admin.middleware.js
@@ -0,0 +1,11 @@
+const isAdmin = (req, res, next) => {
+  try {
+    if (!req.user || !req.user.isAdmin) {
+      return res.status(403).json({ message: 'Access denied. Admins only.' });
+    }
+
+    next();
+  } catch (err) {
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};
